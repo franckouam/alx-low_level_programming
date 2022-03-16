bc7c66323fdae2bab53e6b79464fe6e24e063c9e
Kouam Franck <kouamfranck96@gmail.com>
2022-03-15 18:55:24 -0700

    0x02. C - Functions, nested loops | Mandatory

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - Print the alphabet in lowecase 10 times.
+ * print_alphabet_x10 - Print the alphabet in lowecase 10 times.
  * Return: void.
  */
 void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (j = 0; j < 11; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 97; i < 123; i++)
 		{
