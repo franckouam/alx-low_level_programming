d3615183ba229f937c5c120011ea35cbb5cb8092
Kouam Franck <kouamfranck96@gmail.com>
2022-03-15 19:15:56 -0700

    0x02. C - Functions, nested loops | Mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two integers and returns the result.
+ * @x: The first number.
+ * @y: The second number.
+ * Return: x + y.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -67,4 +67,12 @@ int print_last_digit(int n);
  */
 void jack_bauer(void);
 
+/**
+ * add - Adds two integers and returns the result.
+ * @x: The first number.
+ * @y: The second number.
+ * Return: x + y.
+ */
+int add(int, int);
+
 #endif /* MAIN_H */
