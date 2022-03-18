3e793d3d75a4a78301edf6f0268b59e7d33f261b
Kouam Franck <kouamfranck96@gmail.com>
2022-03-18 02:24:37 -0700

    3. Leap year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -13,9 +13,9 @@ int main(void)
     int day;
     int year;
 
-    month = 1;
+    month = 2;
     day = 29;
-    year = 2007;
+    year = 2000;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
