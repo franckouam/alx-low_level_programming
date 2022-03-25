acd47607c13a52589c195e0d329a2976279184d5
Kouam Franck <kouamfranck96@gmail.com>
2022-03-25 02:54:09 +0100

    0x06. C - More pointers, arrays and strings | 3 review

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,35 +5,36 @@
  */
 int _strlen(char *s)
 {
-	int i = -1;
+	int i = 0;
 	char tmp;
 
 	do {
-		tmp = s[++i];
+		tmp = s[i];
+		i++;
 	} while (tmp != '\0');
-	return (i);
+	return (i - 1);
 }
 
 /**
  * _strcmp - Compare one string to another.
- * @dest: The first string and the destination.
- * @src: The second string.
+ * @s1: The first string and the destination.
+ * @s2: The second string.
  * Return: -1 if s1 < s2
- *          1 if s1 > s2
- *          0 if s1 = s2
+ *	  1 if s1 > s2
+ *	  0 if s1 = s2
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, m = 0, n = 0, res = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
-                m += (int)s1[i++];
-        for (i = 0; s2[i] != '\0'; i++)
-                n += (int)s2[i++];
+		m += (int)s1[i++];
+	for (i = 0; s2[i] != '\0'; i++)
+		n += (int)s2[i++];
 	if (m > n)
-                res = -15;
-        else if(m < n)
-                res = 15;
-        return (res);
+		res = -15;
+	else if (m < n)
+		res = 15;
+	return (res);
 
 }
