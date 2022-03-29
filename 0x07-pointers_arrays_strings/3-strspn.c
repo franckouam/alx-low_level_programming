44facfd94d8a09937d5f1caeef10e24564b3fdfe
Kouam Franck <kouamfranck96@gmail.com>
2022-03-29 22:45:35 +0100

    0x07 | 3.strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,15 +7,17 @@
  */
 int find(char *str, char c)
 {
-        int i = 0, res = -1, end = 0;
+	int i = 0, res = -1;
 
-        do {
-                if (str[i++] == c)
-                        res = i;
-        } while ( res == 0 && end == 0); 
-        if (res == 0 && c == str[i])
-                res = i;
-        return (res);
+	do {
+		if (str[i] == c)
+			res = i;
+		i++;
+	} while (res == -1 && str[i] != '\0');
+	printf("A la fin i vaut %d\n", i);
+	if (res == -1 && c == str[i])
+		res = i;
+	return (res);
 }
 
 /**
@@ -27,14 +29,14 @@ int find(char *str, char c)
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int res = 0;
-	int i, end = 0;
+	int i = 0;
 
 	do {
-		if (find(s, accept[i]))
+		if (find(s, accept[i]) >= 0)
 			res++;
-		if (accept[i] == '\0')
-			end = 1;
 		i++;
-	} while (end == 0);
+	} while (accept[i] != '\0');
+	if (find(s, accept[i]) >= 0)
+		res++;
 	return (res);
 }
