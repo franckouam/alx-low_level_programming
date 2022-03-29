70d82c37473edc676537a12dbc69714e3d4a0be7
Kouam Franck <kouamfranck96@gmail.com>
2022-03-29 03:41:51 +0100

    0x07 | Sum of the diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - Prints the sum of the two diagonals.
+ * @a: The matrix.
+ * @n: The matrix's size.
+ * Description: Prints the sum of the two diagonals of a square
+ *		matrix of integers.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size * size; i = i + size + 1)
+		s1 += a[i];
+	for (i = size - 1; i <= size * size - size; i = i + size - 1)
+		s2 += a[i];
+	printf("%d, %d\n", s1, s2);
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,4 +15,6 @@ char *_strpbrk(char *s, char *accept);
 
 void print_chessboard(char (*a)[8]);
 
+void print_diagsums(int *a, int size);
+
 #endif /* MAIN_H */
