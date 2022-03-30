9ca5ebf20f09e455e988721fd0c8d5b5b26d9c75
Kouam Franck <kouamfranck96@gmail.com>
2022-03-30 23:10:40 +0100

    0x08 | print a string in reverse with recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * _print_rev_recursion - Prints a stringi in reverse.
+ * _print_revrec - Print a string in reverse recursively.
  * @s: The string to print.
  */
-void _print_rev_recursion(char *s)
+void _print_revrec(char *s)
 {
 	if (s[0] == '\0')
 	{
@@ -12,7 +11,17 @@ void _print_rev_recursion(char *s)
 	}
 	else
 	{
-		_print_rev_recursion(s + 1);
+		_print_revrec(s + 1);
 		_putchar(*s);
 	}
+}
+
+/**
+ * _print_rev_recursion - Prints a stringi in reverse.
+ * @s: The string to print.
+ */
+void _print_rev_recursion(char *s)
+{
+	_print_revrec(s);
+	_putchar('\n');
 }
