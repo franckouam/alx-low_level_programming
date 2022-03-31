dcb023722379b22ae30e81b52dda3831c219c66b
Kouam Franck <kouamfranck96@gmail.com>
2022-03-31 01:16:26 +0100

    0x08 | The natural square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+/**
+ * _find_sqrt - Finds the square root of a number.
+ * @n: The number whose the square root will be found.
+ * @to: The limit of testing numbers.
+ * Return: The square root of n if it has.
+ *	   -1 otherwise.
+ */
+int _find_sqrt(int n, int to)
+{
+	if (n < 0 || to < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == to * to)
+		return (to);
+	return (_find_sqrt(n, to - 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number whose the square root will be returned.
+ * Return: The square root of n.
+ */
+int _sqrt_recursion(int n)
+{
+	return (_find_sqrt(n, n / 2));
+}
