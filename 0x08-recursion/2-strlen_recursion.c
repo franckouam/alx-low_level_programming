25eb154dd158ead6ac1503d37974885bc0aa561c
Kouam Franck <kouamfranck96@gmail.com>
2022-03-31 01:52:56 +0100

    0x08 | 2. String length _strlen with recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-
-int _strlen_rec(char *s, int n)
-{
-	if (s[0] == '\0')
-		n = 1;
-	_strlen_rec(s, n++);
-	return (n);
-}
-
 /**
  * _strlen_recursion - Returns the length of a string.
  * @s: The string whose the length will be returned.
