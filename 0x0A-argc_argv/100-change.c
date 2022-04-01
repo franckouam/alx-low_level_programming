f094690a01be8339c25a4ff4d92856d11f424244
Kouam Franck <kouamfranck96@gmail.com>
2022-04-01 18:16:14 +0100

    0x0A. C - argc, argv| Avanced task | A greedy algorithm to print the minimum number of coins to make change of an amount of money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the minimum number of coin for a change.
+ * @argc: The number of console arguments.
+ * @argv: The array of console arguments.
+ * Return: 1 if the program is called without an argument.
+ *	   0 otherwise.
+ *
+ * Description: Prints the minimum number of coins to make
+ *		change for an amount of money.
+ */
+int main(int argc, char *argv[])
+{
+	int r, n = 0, i = 0;
+	int p[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		r = atoi(argv[1]);
+		while (r > 0 && i < 5)
+		{
+			if (r - p[i] >= 0)
+			{
+				printf("Je suis entrer dans le if avec r = %d et i = %d\n", r, i);
+				r = r - p[i];
+				n++;
+			}
+			else
+			{
+				i++;
+			}
+		}
+		printf("Le nombre de pieces a rembourser est: %d\n", n);
+	}
+
+	return (0);
+}
