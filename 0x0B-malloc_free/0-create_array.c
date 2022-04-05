856f4b43903fb3cfc52592557ac264a4d03bbf67
Kouam Franck <kouamfranck96@gmail.com>
2022-04-05 16:40:00 +0100

    0x0B. C - malloc, free | 0. Float like a butterfly. String like a bee.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * create_array - Creates an array of chars.
+ * @size: The size of array.
+ * @c: The char to initialize array with.
+ * Return: A pointer to the array if everything works well.
+ *	   NULL otherwise.
+ */
+ char *create_array(unsigned int size, char c)
+{
+	char *res;
+	unsigned int i;
+	
+	if (size > 0)
+	{
+		res = malloc(size * sizeof(res));
+		if (res != NULL)
+			for (i = 0; i < size; i++)
+				res[i] = c;
+	}
+
+	return (res);
+}
