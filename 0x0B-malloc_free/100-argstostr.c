ab783b2320aaf17aac5d6a4a362d21509cda888e
Kouam Franck <kouamfranck96@gmail.com>
2022-04-06 01:45:32 +0100

    0x0B. C - malloc, free | advanced 1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,12 +26,12 @@ char *argstostr(int ac, char **av)
 
 	if (ac != 0 && av != NULL)
 	{
-		for (i = 1; i < ac; i++)
+		for (i = 0; i < ac; i++)
 			n += _strlen(av[i]);
 		res = malloc((ac + n) * sizeof(char));
 		if (res != NULL)
 		{
-			for (i = 1; i < ac; i++)
+			for (i = 0; i < ac; i++)
 			{
 				n = _strlen(av[i]);
 				for (j = 0; j < n; j++)
