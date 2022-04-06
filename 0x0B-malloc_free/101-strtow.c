8e4f79f63f2153c672f6d6240275d6f90a715538
Kouam Franck <kouamfranck96@gmail.com>
2022-04-06 02:42:34 +0100

    0x0B. C - malloc, free | advanced 2

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,7 +10,7 @@ int count_words(char *str)
 {
 	int i = 0, n = 0;
 
-	for (i = 0; str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 		if (str[i] == 32)
 			n++;
 	return (n);
@@ -24,6 +24,28 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-	int i = 0, n = 0;
+	int i = 0, j = 0, k = 0, n = 0;
+	char **res;
+
+	n = count_words(str);
+	res = malloc(n * sizeof(char *));
+	if (res != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			n = 0;
+			while (str[j++] != 32)
+				n++;
+			j++;
+			res[i] = malloc((n + 1) * sizeof(char));
+			if (res[i] != NULL)
+			{
+				for (k = 0; k < n; k++)
+					res[i][k] = str[j - n - 1 + k];
+				res[i][k] = '\0';
+			}
+		}
+	}
+	return (res);
 }
 
