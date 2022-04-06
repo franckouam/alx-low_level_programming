bb50988f2fd63df90b768bd97950e866f5001827
Kouam Franck <kouamfranck96@gmail.com>
2022-04-06 02:20:18 +0100

    0x0B. C - malloc, free | advanced 1

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,19 +10,27 @@
 int **alloc_grid(int width, int height)
 {
 	int **res = NULL;
-	int i = 0, j = 0;
+	int i = 0, j = 0, e = 0;
 
 	if (width > 0 && height > 0)
 	{
 		res = (int **)malloc(height * sizeof(int *));
 		if (res != NULL)
 		{
-			for (i = 0; i < height; i++)
+			for (i = 0; i < height && e == 0; i++)
 			{
 				res[i] = (int *)malloc(width * sizeof(int));
 				if (res[i] != NULL)
 					for (j = 0; j < height; j++)
 						res[i][j] = 0;
+				else
+					e = 1;
+			}
+			if (e == 1)
+			{
+				for (; i >= 0; i--)
+					free(res[i]);
+				free(res);
 			}
 		}
 	}
