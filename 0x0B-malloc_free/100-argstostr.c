0117ae4b63ca86f92d73c4fbabd4892928a1adde
Kouam Franck <kouamfranck96@gmail.com>
2022-04-06 01:42:36 +0100

    0x0B. C - malloc, free | advanced 1

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string whose the length will be returned.
+ * Return: The length of s.
+ */
+int _strlen(char *s) 
+{
+        if (*s == 0)
+                return (0);
+        else
+                return (1 + _strlen(s + 1));
+}
+
+/**
+ * argstostr - Concatenates all the arguments.
+ * @ac: The number of arguments.
+ * @av: The arguments array.
+ * Return: A string of arguments, each followed by \n.
+ * 	   NULL id ac == 0 or av == NULL.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, k = 0, n = 0;
+	char *res = NULL;
+
+	if (ac != 0 && av != NULL)
+	{
+		for (i = 1; i < ac; i++)
+			n += _strlen(av[i]);
+		res = malloc((ac + n) * sizeof(char));
+		if (res != NULL)
+		{
+			for (i = 1; i < ac; i++)
+			{
+				n = _strlen(av[i]);
+				for (j = 0; j < n; j++)
+					res[k++] = av[i][j];
+				res[k++] = '\n';
+			}
+		}
+	}
+	return (res);
+}
