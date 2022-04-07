ced953978d7bb29920b1d3e1bff6ccb35e0feb36
Kouam Franck <kouamfranck96@gmail.com>
2022-04-07 23:01:42 +0100

    0x0C. C - More malloc, free | Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The size of the memory to allocate.
+ * Return: A pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *res = malloc(b * sizeof(void *));
+
+	if (res == NULL)
+		exit(98);
+	else
+		return (res);
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+# This is the readme file of 0x0C. C - More malloc, free tasks
