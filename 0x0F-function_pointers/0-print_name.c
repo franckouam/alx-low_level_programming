a9b070852c966cb9e9a96e7d3a163efa69d9af31
Kouam Franck <kouamfranck96@gmail.com>
2022-04-14 02:18:17 +0100

    0x0F. C - Function pointers | Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print_name - Prints a name
+ * @name: The name to print
+ * @f: A function that makes changes on a string.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
