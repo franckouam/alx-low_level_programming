663599cd9fd3440d58e202df21b6b593424036fd
Kouam Franck <kouamfranck96@gmail.com>
2022-04-28 00:08:37 +0100

    0x13. C - More singly linked lists | task 9 review

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				tmp = tmp->next;
 				i++;
 			}
-			if (i  < idx)
+			if (i + 1  < idx)
 			{
 				node->next = tmp->next;
 				tmp->next = node;
