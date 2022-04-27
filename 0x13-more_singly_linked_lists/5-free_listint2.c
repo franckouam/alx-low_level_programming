747ac13da248dfca83096d231b8ed108aaaa60a0
Kouam Franck <kouamfranck96@gmail.com>
2022-04-27 22:15:16 +0100

    0x13. C - More singly linked lists | task 5 review

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while ((*head)->next != NULL)
+	if(*head)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		while ((*head)->next != NULL)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			free(tmp);
+		}
+		free(*head);
+		*head = NULL;
 	}
-	free(*head);
-	*head = NULL;
 }
