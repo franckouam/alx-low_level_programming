85bd0031cc766916eae5d5c08950a3f229b6c243
Kouam Franck <kouamfranck96@gmail.com>
2022-10-05 17:52:46 +0100

    0x1E. | 0. Linear search - Testing the checker

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,29 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "search_algos.h"
 
-/**
- * linear_search - Searches a value in an array using a linear search.
- * @array: The array to search in.
- * @size: The length of the array.
- * @value: The value to look for.
- *
- * Return: The first index of the value in the array, otherwise -1.
- */
 
+/**
+ * linear_search - Linear search algorithm implementation
+ * @array: The array to search within
+ * @size: The length of the array
+ * @value: The value to search
+ * Return: The first index where `value` is located if it is in `array`
+ *         -1 otherwise.
+ */
 int linear_search(int *array, size_t size, int value)
 {
+	int found = -1;
 	size_t i;
 
-	for (i = 0; (i < size) && (array); i++)
+	if (array)
 	{
-		if (*(array + i) == value)
+		i = 0;
+		while (i < size && found == -1)
 		{
-			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
-			return (i);
-		}
-		else
-		{
-			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+			printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+			if (array[i] == value)
+				found = array[i];
+			i++;
 		}
 	}
-	return (-1);
+	return (found);
+
 }
