8e28798a357606f35387bf1331f2f968aceb6789
Kouam Franck <kouamfranck96@gmail.com>
2022-10-05 17:36:02 +0100

    0x1E. | 0. Linear search - Using the stdlib printf function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
+#include <stdio.h>
 #include "search_algos.h"
 
 
@@ -22,11 +21,7 @@ int linear_search(int *array, size_t size, int value)
 		i = 0;
 		while (i < size && found == -1)
 		{
-			print_str("Value checked array[");
-			_putint(i);
-			print_str("] = [");
-			_putint(array[i]);
-			print_str("]\n");
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 				found = array[i];
 			i++;
@@ -34,93 +29,4 @@ int linear_search(int *array, size_t size, int value)
 	}
 	return (found);
 
-}
-
-
-/**
- * print_str - Prints a string
- * @str: the string to print
- * Return: void.
- */
-void print_str(char *str)
-{
-	char tmp;
-	int i = 0;
-
-	tmp = str[0];
-	while (tmp != '\0')
-	{
-		_putchar(tmp);
-		tmp = str[++i];
-	}
-}
-
-/**
- * _putint - Prints an integer.
- * @n: The integer to print.
- * Return: The number of digit printed.
- */
-int _putint(int n)
-{
-	int tmp, i = 1, j = 0, r = 0, abs_n = n;
-
-	if (n < 0)
-	{
-		if (n == INT_MIN)
-		{
-			abs_n = INT_MAX;
-			r = 1;
-		}
-		else
-		{
-			abs_n = -n;
-			_putchar('-');
-		}
-	}
-	tmp = abs_n;
-	while (tmp >= 10)
-	{
-		tmp = tmp / 10;
-		i++;
-	}
-	for (j = i; j > 0; j--)
-	{
-		if (j == 10)
-			tmp = (abs_n / _pow(10, 9)) % 10;
-		tmp = (abs_n % _pow(10, j)) / _pow(10, j - 1);
-		if (j == 1)
-			tmp += r;
-		_putchar(48 + tmp);
-	}
-	return (i);
-
-}
-
-/**
- * _pow - Computes x raised to power of y.
- * @x: The mantisse.
- * @y: The exposant.
- * Return: x ^ y.
- */
-int _pow(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	else if (y == 1)
-		return (x);
-	return (x * _pow(x, y - 1));
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
